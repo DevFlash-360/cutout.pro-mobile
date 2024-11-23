90d2d6c0888c233d463fc296a1ef85c1d8ccf591
DaniciaStoryboard <daniv112110@gmail.com>
2024-11-24 04:40:20 +0900

    Login Screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="login/index" options={{
+        headerShown: false
+      }} />
+    </Stack>
+  )
 }
